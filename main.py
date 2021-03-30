aea76554ac7f3036632e88fb63dbb2fa2a07af60
Serhii Alieksieiev <se.alieksieiev@gmail.com>
2021-03-30 18:04:05 +0300

    Connect DialogFlow

--- main.py
+++ main.py
@@ -4,6 +4,23 @@ import logging
 
 import telegram
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from google.cloud import dialogflow, storage
+
+def textMessage(update, context):
+    text = update.message.text
+    project_id = os.environ['GCP_PROJECT_ID']
+    session_id = 123456789
+    session_client = dialogflow.SessionsClient()
+    session = session_client.session_path(project_id, session_id)
+    text_input = dialogflow.TextInput(text=text, language_code="ru")
+    query_input = dialogflow.QueryInput(text=text_input)
+    response = session_client.detect_intent(
+        request={"session": session, "query_input": query_input}
+    )
+    response = response.query_result.fulfillment_text
+    
+    context.bot.send_message(
+        chat_id=update.effective_chat.id, text=response)
 
 
 def start(update, context):
@@ -11,33 +28,24 @@ def start(update, context):
         chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
 
 
-def echo(update, context):
-    context.bot.send_message(
-        chat_id=update.effective_chat.id, text=update.message.text)
-
-
 def main():
     dotenv.load_dotenv('.env')
     telegram_token = os.environ['TELEGRAM_TOKEN']
-    # Проверка бота
-    # print(bot.get_me())
-    # Настройки
+    
     updater = Updater(token=telegram_token, use_context=True)
     dispatcher = updater.dispatcher
-    # Хендлеры
+
     start_handeler = CommandHandler('start', start)
-    echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
+    text_message_handler = MessageHandler(Filters.text, textMessage)
 
     logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
                         level=logging.INFO)
-    # Добавляем хендлеры в диспетчер
-    dispatcher.add_handler(start_handeler)
-    dispatcher.add_handler(echo_handler)
 
-    # Начинаем поиск обновлений
+    dispatcher.add_handler(start_handeler)
+    dispatcher.add_handler(text_message_handler)
+
     updater.start_polling()
 
-    # Останавливаем бота, если были нажаты Ctrl + C
     updater.idle()
 
 
