90fb15c34393bf828ccd44b9bb8fd77cd3722142
Serhii Alieksieiev <se.alieksieiev@gmail.com>
2021-04-01 10:37:36 +0300

    Add training code for Dialogflow

---
+++ training.py
@@ -0,0 +1,51 @@
+import os
+import dotenv
+import json
+
+from google.cloud import dialogflow
+
+
+def create_intent(project_id, display_name, training_phrases_parts, message_texts):
+    """Create an intent of the given intent type."""
+    from google.cloud import dialogflow
+
+    intents_client = dialogflow.IntentsClient()
+
+    parent = dialogflow.AgentsClient.agent_path(project_id)
+    training_phrases = []
+    for training_phrases_part in training_phrases_parts:
+        part = dialogflow.Intent.TrainingPhrase.Part(
+            text=training_phrases_part)
+        # Here we create a new training phrase for each provided part.
+        training_phrase = dialogflow.Intent.TrainingPhrase(parts=[part])
+        training_phrases.append(training_phrase)
+
+    text = dialogflow.Intent.Message.Text(text=message_texts)
+    message = dialogflow.Intent.Message(text=text)
+
+    intent = dialogflow.Intent(
+        display_name=display_name, training_phrases=training_phrases, messages=[
+            message]
+    )
+
+    response = intents_client.create_intent(
+        request={"parent": parent, "intent": intent}
+    )
+
+    print("Intent created: {}".format(response))
+
+
+def main():
+    dotenv.load_dotenv(".env")
+    project_id = os.environ["GCP_PROJECT_ID"]
+
+    with open("questions.json", "r", encoding="utf8") as my_file:
+        questions = json.load(my_file)
+
+    for title, intent in questions.items():
+        create_intent(project_id, title,
+                      intent['questions'], [intent['answer']])
+
+
+if __name__ == '__main__':
+    main()
