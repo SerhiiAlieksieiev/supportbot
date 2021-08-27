597ee55d0822a1d09abb413e301e606091f682e1
Serhii Alieksieiev <se.alieksieiev@gmail.com>
2021-08-27 20:39:41 +0300

    optimize logs_handler.py

--- logs_handler.py
+++ logs_handler.py
@@ -6,9 +6,9 @@ class LogsHandler(logging.Handler):
     def __init__(self, token, chat_id):
         super().__init__()
         self.chat_id = chat_id
-        self.token = token
+        self.bot = telegram.Bot(token)
 
     def emit(self, record):
         log_entry = self.format(record)
-        tg_bot = telegram.Bot(self.token)
+        tg_bot = self.bot
         tg_bot.send_message(chat_id=self.chat_id, text=log_entry)
